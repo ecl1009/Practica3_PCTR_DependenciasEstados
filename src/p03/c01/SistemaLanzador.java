7091c9751e42d45299c2896fc1fc3eb6dc14c04d
iau1001 <118663977+iau1001@users.noreply.github.com>
2023-03-21 20:27:19 +0100

    Completar clase SistemaLanzador #7
    Se completa la clase SistemaLanzador añadiendo 10 hilos, 5 para la entrada del parque y 5 para la salida del parque. Se comenta el código y se añade la documentación Javadoc.

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -1,26 +1,48 @@
 package src.p03.c01;
 
+/**
+ * Clase SistemaLanzador - Lanza los Thread correspondientes a cada proceso de entrada/salida de cada puerta del parque.
+ * 
+ * @author Irati Arraiza Urquiola - Eduardo Manuel Cabeza Lopez
+ * @version 1.0
+ * @since 1.0
+ * @see ActividadEntradaPuerta
+ * @see ActividadSalidaPuerta 
+ * @see Parque
+ */
 public class SistemaLanzador {
+	
+	/**
+	 * Método principal main - Lanza 10 Threads, 5 de entrada y 5 de salida del parque.
+	 * 
+	 * Cada Thread de entrada o salida corresponde a una puerta del parque, teniendo en total 5 puertas, cada una con su
+	 * proceso o actividad de entrada y salida.
+	 * 
+	 * @param args Argumentos de entrada en al ejecución
+	 */
 	public static void main(String[] args) {
 		
-		IParque parque = new Parque(); // TODO
-		char letra_puerta = 'A';
+		IParque parque = new Parque(); // Parque para el que se lanzan los hilos 
+		char letra_puerta = 'A'; // Letra inicial para asignar nombres a cada puerta
+		final int NUMPUERTAS = 5; // Número de peurtas del parque
 		
-		System.out.println("¡Parque abierto!");
+		// Mensaje que indica que el parque ha abierto
+		System.out.println("Parque abierto:\n");
 		
-		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
-			
-			String puerta = ""+((char) (letra_puerta++));
-			
+		// Bucle que se repite tantas veces como puertas tenga el parque
+		for (int i = 0; i < NUMPUERTAS; i++) {
+
+			String puerta = "" + ((char) (letra_puerta++));
+
 			// Creación de hilos de entrada
 			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
-			new Thread (entradas).start();
-			
-			// 
-			// TODO
-			//
-			
-			
+			new Thread(entradas).start();
+					
+			// Creación de hilos de salida
+			ActividadSalidaPuerta salidas = new ActividadSalidaPuerta(puerta, parque);
+			new Thread(salidas).start();
+					
 		}
+			
 	}	
 }
