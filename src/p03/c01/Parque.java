68aed9c2ae11e8127bc5bbea854aa2567e8b08bd
iau1001 <118663977+iau1001@users.noreply.github.com>
2023-03-21 20:00:54 +0100

    Contadores totales por puerta y completar método que imprime la información #5
    Se han añadido contadores para el total de entradas y salidas por cada puerta. Se modifica el formato de impresión del método imprmirInfo para que muestre los valores totales y los actuales del parque y cada puerta. En los métodos de entrada y salida se modifican estos valores para actualizar el estado del parque.

--- src/p03/c01/Parque.java
+++ src/p03/c01/Parque.java
@@ -22,6 +22,9 @@ public class Parque implements IParque{
 	 * Cantidad de personas actuales en el parque.
 	 */ 
 	private int contadorPersonasTotales;
+	/**
+	 * Balance de entradas/salidas por una puerta del parque.
+	 */
 	private Hashtable<String, Integer> contadoresPersonasPuerta;
 	/**
 	 * Aforo máximo del parque.
@@ -31,11 +34,47 @@ public class Parque implements IParque{
 	 * Cantidad mínima de personas en el parque. 
 	 */
 	private final int AFOROMIN = 0; 
+	/**
+	 * Tiempo medio.
+	 */
+	private double tmedio; 
+	/**
+	 * Tiempo inicial.
+	 */
+	private double tinicial; 
+	/**
+	 * Tiempo final.
+	 */
+	private double tfinal; 
+	/**
+	 * Total de entradas al parque.
+	 */
+	private int totalEntradas; 
+	/**
+	 * Total de salidas del parque.
+	 */
+	private int totalSalidas; 
+	/**
+	 * Total de entradas por puerta.
+	 */
+	private Hashtable<String, Integer> entradasPuertas;
+	/**
+	 * Total de salidas por puerta.
+	 */
+	private Hashtable<String, Integer> salidasPuertas;  
 	
-	
+	/**
+	 * Constructor de la clase.
+	 * 
+	 * Inicializa los atributos de contadores.
+	 */
 	public Parque() {
 		contadorPersonasTotales = 0;
 		contadoresPersonasPuerta = new Hashtable<String, Integer>();
+		entradasPuertas = new Hashtable<String, Integer>();
+		salidasPuertas = new Hashtable<String, Integer>();
+		totalEntradas = 0;
+		totalSalidas = 0;
 	}
 
 	/**
@@ -52,21 +91,27 @@ public class Parque implements IParque{
 		// Comprobar precondiciones
 		comprobarAntesDeEntrar();
 		
-		// Si no hay entradas por esa puerta, inicializamos
-		if (contadoresPersonasPuerta.get(puerta) == null){
-			contadoresPersonasPuerta.put(puerta, 0);
-		}
-			
-		// Aumentamos el contador total y el individual
-		contadorPersonasTotales++;		
-		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta)+1);
-		
+		// Si no hay salidas ni entradas por esa puerta, inicializamos
+        if (contadoresPersonasPuerta.get(puerta) == null) {
+            contadoresPersonasPuerta.put(puerta, 0);
+            entradasPuertas.put(puerta, 0);
+            salidasPuertas.put(puerta, 0);
+           
+        }
+        // Se obtiene el tiempo inicial.
+        tinicial = System.currentTimeMillis();
+        //Aumenta en uno las personas actuales.
+      	contadorPersonasTotales++;
+      	//Aumenta en uno el total de entradas del parque.
+      	totalEntradas++;
+      	//Aumenta en uno el balance de entradas/salidas por la puerta. 
+      	contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta) + 1);
+      	//Aumenta en uno el total de entradas por la puerta. 
+      	entradasPuertas.put(puerta, entradasPuertas.get(puerta) + 1);
 		// Imprimimos el estado del parque
 		imprimirInfo(puerta, "Entrada");
-		
 		// Se notifica a todos los hilos.
-				notifyAll();
-		
+		notifyAll();
 		// Se comprueba el invariante. 
 		checkInvariante();
 	}
@@ -84,34 +129,59 @@ public class Parque implements IParque{
 		// Comprobar precondiciones
 		comprobarAntesDeSalir();
 		
-		// Si no hay salidas por esa puerta, inicializamos
-		if (contadoresPersonasPuerta.get(puerta) == null){
-			contadoresPersonasPuerta.put(puerta, 0);
-		}
+		// Si no hay salidas ni entradas por esa puerta, inicializamos
+        if (contadoresPersonasPuerta.get(puerta) == null) {
+            contadoresPersonasPuerta.put(puerta, 0);
+            entradasPuertas.put(puerta, 0);
+            salidasPuertas.put(puerta, 0);
+           
+        }
+        // Se obtiene el tiempo final.
+     	tfinal = System.currentTimeMillis();
+     	// Se obtiene el tiempo medio.
+     	tmedio = (tmedio +(tfinal - tinicial))/2;
+     	//Disminuye en uno las personas actuales.
+     	contadorPersonasTotales--;
+     	//Aumenta en uno el total de salidas del parque.
+     	totalSalidas++;
+     	//Disminuye en uno el balance de entradas/salidas por la puerta. 
+     	contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta) - 1);
+     	//Aumenta en uno el total de salidas por la puerta. 
+     	salidasPuertas.put(puerta, salidasPuertas.get(puerta) + 1);
 				
-		
-		// Aumentamos el contador total y el individual
-		contadorPersonasTotales--;		
-		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta)-1);
 		
 		// Imprimimos el estado del parque.
 		imprimirInfo(puerta, "Salida");
-		
 		// Se notifica a todos los hilos.
 		notifyAll();
-		
 		// Se comprueba el invariante. 
 		checkInvariante();
 	}
 	
-	
+	/**
+	 * Método imprimirInfo - Imprime la información del estado actual del parque,
+	 * 
+	 * Imprime el estado del parque en el momento actual. Se muestra la información del número total de personas
+	 * en el parque, el total de entradas, el total de salidas y el tiempo medio de estancia en el parque.
+	 * Imprime en formato tabla la información de las entradas, salidas y el balance total de entradas y salidas por
+	 * cada puerta del parque.
+	 * 
+	 * 
+	 * @param puerta Puerta que actualiza el estado del parque
+	 * @param movimiento Tipo de movimiento (Entrada/Salida)
+	 */
 	private void imprimirInfo (String puerta, String movimiento){
 		System.out.println(movimiento + " por puerta " + puerta);
-		System.out.println("--> Personas en el parque " + contadorPersonasTotales); //+ " tiempo medio de estancia: "  + tmedio);
-		
+		System.out.println("--> Personas en el parque " + contadorPersonasTotales );
+		System.out.println("--> Total entradas: " + totalEntradas + "\t total salidas: " + totalSalidas + " \tTiempo medio de estancia: " + tmedio);
+		System.out.println("");
+		System.out.println("----> Puerta\tTotal entradas\tTotal salidas\tBalance");
+
 		// Iteramos por todas las puertas e imprimimos sus entradas
-		for(String p: contadoresPersonasPuerta.keySet()){
-			System.out.println("----> Por puerta " + p + " " + contadoresPersonasPuerta.get(p));
+		for (String p : contadoresPersonasPuerta.keySet()) {
+			System.out.println("---->   " + p + "\t\t" + entradasPuertas.get(p) + "\t\t" + salidasPuertas.get(p) + "\t   " + contadoresPersonasPuerta.get(p));
+			//System.out.println("----> " + p + " entradas: " + entradasPuertas.get(p) + ", salidas: "
+					//+ salidasPuertas.get(p));
 		}
 		System.out.println(" ");
 	}
