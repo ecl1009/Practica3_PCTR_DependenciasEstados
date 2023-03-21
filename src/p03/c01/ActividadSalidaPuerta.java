d46e03dd14dd53b6458f3775ff21231d35124cb5
iau1001 <118663977+iau1001@users.noreply.github.com>
2023-03-21 18:50:17 +0100

    Añadir método run e imports #1
    Implementar el método run de la interfaz Runnable y añadir los imports.

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,5 +1,8 @@
 package src.p03.c01;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Clase ActividadSalidaPuerta - Simula las salidas del parque.
  * 
@@ -12,7 +15,7 @@ package src.p03.c01;
  * @since 1.0
  *
  */
-public class ActividadSalidaPuerta{
+public class ActividadSalidaPuerta implements Runnable{
 
 	/**
 	 * Número de salidas por la puerta
@@ -32,7 +35,7 @@ public class ActividadSalidaPuerta{
 	/**
 	 * Constructor de la clase.
 	 *
-	 * Inicializa los atributos puerta y parque con los pámetros pasados al constructor.
+	 * Inicializa los atributos puerta y parque con los parámetros pasados al constructor.
 	 * 
 	 * @param puerta String con el nombre de la puerta por la que se simulan las salidas.
 	 * @param parque Objeto de clase Parque que implementa la interfaz IParque.
@@ -40,5 +43,31 @@ public class ActividadSalidaPuerta{
 	public ActividadSalidaPuerta(String puerta, IParque parque) {
 		this.puerta = puerta;
 		this.parque = parque;
+	}
+	
+	/**
+	 * Método run - Método que ejecuta el hilo al ser creado.
+	 * Simula NUMSALIDAS salidas del parque.
+	 */
+	@Override
+	public void run() {
+		// Se ejecuta el bucle hasta alcanzar el número máximo de salidas establecido en NUMSALIDAS
+		for (int i = 0; i < NUMSALIDAS; i++) {
+			// Bloque try que se intenta ejecutar
+			try {
+				//Invoca al método salirDelParque pasandole la puerta como parámetro
+				parque.salirDelParque(puerta);
+				//Duerme al hilo un tiempo aleatorio entre 0 y 5 segundos
+				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5) * 1000);
+				
+				//Captura la excepción InterruptedException cuando se interrumpe el hilo
+			} catch (InterruptedException e) {
+				// Al capturar la excepción se imprime un mensaje informando de la excepción capturada
+				System.out.println("Salida interrumpida");
+				System.out.println(e.toString());
+				return; // Finaliza la ejecución del método
+			}
+
+		}
 	}
 }
