ecf0886398b5f9f76bcb7b5abcb10348fb4832d5
iau1001 <118663977+iau1001@users.noreply.github.com>
2023-03-21 18:56:31 +0100

    Añadir método abstracto salirDelParque #3
    Añadir el método abstracto salirDelParque y la documentación Javadoc.

--- src/p03/c01/IParque.java
+++ src/p03/c01/IParque.java
@@ -1,9 +1,25 @@
 package src.p03.c01;
 
+/**
+ * Interfaz IParque - Proporciona el esqueleto para los métodos de entrada y salida de un parque. 
+ * 
+ * @author Irati Arraiza Urquiola - Eduardo Manuel Cabeza Lopez
+ * @version 1.0
+ * @since 1.0
+ *
+ */
 public interface IParque {
 	
+	/**
+	 * Simula la entrada a un parque. 
+	 * @param puerta Puerta por la que se entra.
+	 */
 	public abstract void entrarAlParque(String puerta);
 
-	// TODO
+	/**
+	 * Simula la salida de un parque.
+	 * @param puerta Puerta por la que se sale. 
+	 */
+	public abstract void salirDelParque(String puerta);
 
 }
